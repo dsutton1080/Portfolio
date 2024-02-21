e5fbd883f73e71c39b61f7763bec0396871e41d4
DeRon Sutton <dsutton1080@gmail.com>
2024-02-20 23:03:51 -0600

    Migrate from SQLite to Mongo DB

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -45,9 +45,10 @@ export default async function ExperienceList() {
     >
       <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
         <div className="flex max-w-3xl flex-col space-y-16">
-          {experiences.map((experience) => (
-            <Experience key={experience.title} experience={experience} />
-          ))}
+          {experiences &&
+            experiences.map((experience) => (
+              <Experience key={experience.title} experience={experience} />
+            ))}
         </div>
       </div>
     </SimpleLayout>
--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -32,11 +32,12 @@ export default async function ResumeList() {
     <SimpleLayout title="Resume" intro="">
       <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
         <div className="flex max-w-3xl flex-col space-y-16">
-          {Object.values(resume)
-            .flat()
-            .map((section, index) => (
-              <Section key={index} section={section} />
-            ))}
+          {resume &&
+            Object.values(resume)
+              .flat()
+              .map((section, index) => (
+                <Section key={index} section={section} />
+              ))}
         </div>
       </div>
     </SimpleLayout>
